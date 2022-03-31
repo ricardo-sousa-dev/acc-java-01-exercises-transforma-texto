0cdb5c812e4216f196480af137ae3f1cc62e4839
ricardo-sousa <rwmsousa@gmail.com>
2022-03-31 09:23:45 -0300

    feat - resolve exercise

--- src/main/java/com/trybe/acc/java/transformatexto/Texto.java
+++ src/main/java/com/trybe/acc/java/transformatexto/Texto.java
@@ -1,5 +1,18 @@
 package com.trybe.acc.java.transformatexto;
 
+/** Sistema de processamento de texto. */
 public class Texto {
+  /** Processa o texto. */
+  public String transformaTexto(String text) {
+    String removeSimbol = text.replace("+", " ");
 
+    if (text.indexOf("a") % 2 == 0) {
+      // se a primeira letra "a" encontrada estiver em posição par, retorna o texto em
+      // caixa alta.
+      return removeSimbol.toUpperCase();
+    } else {
+      // retorna a primeira palavra encontrada
+      return removeSimbol.split(" ")[0];
+    }
+  }
 }
